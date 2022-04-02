f230a184415940be40a6453d7f4d6264929605b5
Derricka S <derrickas728@gmail.com>
2022-04-02 19:01:03 -0400

    complete delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+var pageContentEl = document.querySelector("#page-content");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 var taskIdCounter = 0;
+
 
 var taskFormHandler = function(event){
     event.preventDefault();
@@ -77,8 +80,23 @@ var createTaskActions = function(taskId){
 
     return actionContainerEl;
     
+};
+
+var taskButtonHandler = function(event){
+    console.log(event.target);
+
+    if(event.target.matches(".delete-btn")){
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
 }
 
-
 formEl.addEventListener("submit", taskFormHandler);
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
